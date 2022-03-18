1300e946357aa0fe981bb6a4efb172ef6d45b0f2
ZacharyGoolman <zachary.goolman@gmail.com>
2022-03-18 14:34:52 -0400

    added class Alarm clock file to create OOP

---
+++ alarm_clock.py
@@ -0,0 +1,20 @@
+class Alarm_clock:
+    def __init__ (self, time, power_on, alarm_time):
+        self.time = ("")
+        self.power_on = False
+        self.alarm_time = ("")
+
+        def create_time(self):
+            self.time = ("12:00")
+            change_time =
+        
+        
+        def check_power(self):
+            if self.power_on == True:
+                print(f"Clock power is on and Tesla fueled. It is currently {time}")
+            else:
+                print("This sucka aint got no powaaaa")
+
+        def create_alarm(self):
+            self.alarm_time = input("What time you wanna wake up sleepy head?")
+            print(alarm_time)
---
+++ main.py
@@ -0,0 +1,23 @@
+                            #create alarm clock class
+
+#want to be able to keep track of the time of an alarm clock, keep this in a string format
+    #method to set current time
+
+#want to be able to change the time of the alarm clock and print that to the terminal
+    #method to be able to change the time
+
+#I want to be able to turn the alarm clock on or off
+    # method to be able to alarm clock power
+
+#want to be able to create an alarm with a time and print to terminal that alarm time
+    # method to set time for alarm
+
+#I want to be able to import my alarm clock class to a main.py file as an object and call the methods that i have created for it. 
+    #1 print clocks time to terminal 
+    #2 call alarm clocks change time method 
+    #3toggle the alarm clock on off switch
+
+
+from alarm_clock import Alarm_clock
+
+
